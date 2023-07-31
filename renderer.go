67a79c9e52362d0d2f04434a8ea1af126c9ddff5
MikeFilimonov <filimonov.michael@gmail.com>
2023-07-31 17:16:56 +0300

    upgraded the page view

---
+++ renderer.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// Renders a page from a templage
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
