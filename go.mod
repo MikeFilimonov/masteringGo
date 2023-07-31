b08f753d86c18ede3637170333111120f7337ed8
MikeFilimonov <filimonov.michael@gmail.com>
2023-07-31 09:42:02 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hww
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the homepage")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+
+	sum := addValues(4, 5)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("The AddValues result is %d", sum))
+}
+
+// Divide returns the quotient of two float32
+func Divide(w http.ResponseWriter, r *http.Request) {
+
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+
+		fmt.Fprintf(w, "Cannot divide by zero.")
+		return
+
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf(" %f divided by %f is %f", 100.0, 0.0, f))
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+
+}
+
+// divideValues divides two floats and returns the result and the possible error
+func divideValues(x, y float32) (float32, error) {
+
+	if y == 0 {
+
+		err := errors.New("Can't divide by 0")
+		return 0, err
+
+	}
+
+	result := x / y
+	return result, nil
+
+}
+
+// main is the main entry point of the app
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+
+	fmt.Println(fmt.Sprintf("Starting the app at port %s", portNumber))
+
+	_ = http.ListenAndServe(portNumber, nil)
+
+}
