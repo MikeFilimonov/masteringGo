59d2a62947abef798812d713e17947d9986a6696
MikeFilimonov <filimonov.michael@gmail.com>
2023-09-17 12:17:30 +0300

    middleware usage sample

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,15 +5,18 @@ import (
 
 	"github.com/MikeFilimonov/masteringGo/pkg/config"
 	"github.com/MikeFilimonov/masteringGo/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
